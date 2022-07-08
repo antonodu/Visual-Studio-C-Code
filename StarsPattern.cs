b93f669778ace8a49e9797e9f402c09e97fd64e7
antonodu <antonodu@hotmail.co.uk>
2022-07-08 11:56:33 +0100

    Update StarsPattern.cs
    stars pattern change

--- StarsPattern.cs
+++ StarsPattern.cs
@@ -1,31 +1,17 @@
-﻿namespace StarsPattern
+namespace StarsPattern
 {
     class Program
     {
         static void Main(string[] args)
+       
         {
-            int i, j;
-            int space = 10;
-            int MAX = space;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
 
-            for (i = 0; i < MAX; i++)
-            {
-                for (j = 0; j < space; j++)
-                {
-                    Console.WriteLine(" ");
-                }
-                {
-                    for (j = 0; j <= i; j++) ;
-                }
-                {
-                    Console.WriteLine("* ");
-                }
-
-                Console.Write("\n");
-              
-            }
             Console.ReadLine();
         }
     }
 }
-
