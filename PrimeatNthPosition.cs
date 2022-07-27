3ed3e9ba722a953d07879e473f55769b5178b10e
antonodu <antonodu@hotmail.co.uk>
2022-07-27 13:33:32 +0100

    PrimeatNthPosition

---
+++ PrimeatNthPosition.cs
@@ -0,0 +1,34 @@
+﻿namespace PrimeatNthPosition
+{
+    class Program
+    {
+        static void Main( string[] args)
+        {
+            Console.WriteLine("Please enter input number:");
+            int input = int.Parse( Console.ReadLine() );
+            if(input <= 1)
+            {
+                Console.WriteLine("This is an invalid number");
+            }
+            else
+            {
+                int flag = 0;
+                for (int i = 2; i < input; i++)
+                {
+                    if(input%i == 0)
+                    {
+                        Console.WriteLine(" {0} is not a prime number", input);
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    Console.WriteLine("{0} is a prime number", input);
+                }
+            }
+        }
+    }
+}
+            
+   
